85499ce8b07a05823c33230709b7bfd42ab73f78
Khatchi <solomon9ice2014@gmail.com>
2023-10-16 08:08:17 -0700

    ....A script that sets up your web servers for the deployment of web_static

---
+++ 1-pack_web_static.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+Fabric script that generates a .tgz archive.
+"""
+
+from fabric.api import task, local
+from datetime import datetime
+
+
+@task
+def do_pack():
+    """ usage:
+        sudo fab -f 1-pack_web_static.py do_pack
+    """
+    formatted_dt = datetime.now().strftime('%Y%m%d%H%M%S')
+    mkdir = "mkdir -p versions"
+    path = "versions/web_static_{}.tgz".format(formatted_dt)
+    print("Packing web_static to {}".format(path))
+    if local("{} && tar -cvzf {} web_static".format(mkdir, path)).succeeded:
+        return path
+    return None
